4767ae6bce3dc178eb0227828309e24c834ce2ea
mbarbosasan <moisesbarbosa23@gmail.com>
2023-10-10 21:54:25 -0300

    chore: removendo dependencias externas e inserindo as localmente.

--- ng-project/src/app/app-routing.module.ts
+++ ng-project/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {APP_BASE_HREF} from "@angular/common";
 import {EmptyRouteComponent} from "./empty-route/empty-route.component";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
-  { path: "**", component: EmptyRouteComponent}
+  { path: "**", component: EmptyRouteComponent},
+  { path: 'ng', component: AppComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/poc-root-config.js
+++ src/poc-root-config.js
@@ -1,12 +1,14 @@
 import { registerApplication, start } from "single-spa";
 import singleSpaAngularJS from 'single-spa-angularjs'
+import angular from 'angular'
+
 
 angular.module('app', [])
 
 angular
     .module('app')
     .component('root', {
-      template: '<p> Hello World AngularJS</p>',
+      template: `<p> Hello World AngularJS</p><a href="/ng">Retornar ao Angular 13</a>`,
       controller: function () {
         this.$onInit = function () {
           console.log("AngularJS Bootstrapped")
@@ -27,11 +29,11 @@ window.myAngularApp = singleSpaAngularJS({
   template: '<root/>'
 })
 
-registerApplication({
-  name: "angular",
-  app: () => System.import("http://localhost:4200/main.js"),
-  activeWhen: ["/"]
-});
+registerApplication(
+  "angular",
+  () => System.import("http://localhost:4200/main.js"),
+  location => location.pathname.startsWith('/ng') && !location.pathname.includes('/js')
+);
 
 registerApplication({
   name: '@angularjs',
